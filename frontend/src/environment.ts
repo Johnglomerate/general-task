9d5f0caee54763b3ddcef08d2134cbd372d998fa
yohanstra <jreinstra@gmail.com>
2026-09-09 18:45:00 -0700

    Update General Task landing page trial copy

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -10,7 +10,7 @@ const ENV = {
         REACT_APP_API_BASE_URL: 'https://api.generaltask.com',
         REACT_APP_FRONTEND_BASE_URL: 'https://generaltask.com',
         REACT_APP_TASK_BASE_URL: 'https://share.generaltask.com',
-        REACT_APP_TRY_BASE_URL: 'https://generaltask.com',
+        REACT_APP_TRY_BASE_URL: 'https://try.generaltask.com',
         COOKIE_DOMAIN: '.generaltask.com',
     },
 }
